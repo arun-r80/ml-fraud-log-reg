0142d29547c8543720e583475077b6b8b2d28fab
arun-r80 <arun_r80@yahoo.co.in>
2018-05-27 19:49:25 +0530

    refactored

--- source/ml/trainmodel.py
+++ source/ml/trainmodel.py
@@ -4,20 +4,12 @@ Created on Fri May 25 07:24:03 2018
 
 @author: aruramam
 """
-
 import pandas as pd
-import numpy as np
-import os
 from datetime import datetime
-import pathlib as pathlib
 from sklearn.feature_selection import RFE
 from sklearn.svm import SVC
 from sklearn.externals import joblib
-from sklearn.linear_model import LogisticRegression
-from config_model import CONFIG
 from loadtrainingdata import _get_training_data_
-
-
 #################################################################
 #################################################################
 #################################################################
@@ -69,43 +61,6 @@ print('training data pickled')
 print(selector.get_support(indices=True))
 print(selector.get_support(indices=False))
 #
-#featureselection = selector_pickle.get_support(indices=False)
-#print(featureselection)
-##featureselection = selector.get_support(indices=False)
-### Print selected features
-#print('All features')
-#print('###################')
-#print('###################')
-#for column,series in trainingdata.iteritems():
-#    print(column)
-#print('###################')
-#print('###################')
-#print('selected features:')
-#
-#i=0
-#for column,series in trainingdata.iteritems():
-#    if (featureselection[i] == True ):
-#        print(column)
-#    i+=1
-### pickle the selector 
-##joblib.dump(selector,'rfe.pkl')
-#c=[x for x,status in zip(trainingdata.columns.values,featureselection) if status == True ]
-#print('pickled selector')
-#training_selected_features = trainingdata.loc[featureselection]
-#print('treated data list')
-#print(trainingdata[c])
-###fit data to Logistic regression
-#regressor = LogisticRegression(C=1.0,fit_intercept = True,solver='liblinear')
-#startregression = datetime.now()
-#print("start regression fitting ....")
-#regressor = regressor.fit(trainingdata[c],y)
-#endregression = datetime.now()
-#print('end regression fitting ....')
-#print('time taken for fitting')
-#print(endregression - startregression)
-#print(regressor.coef_)
-
-# =============================================================================
 
 
 
